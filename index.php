c6947287f45b818720228bc1e9ae2b3457645424
Milinko Dragovic <Milinko Dragovic>
2016-11-22 21:08:59 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Banik - Svijet Kupatila</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<!-- Top Nav Bar -->
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <a href="index.php" class="navbar-brand">Banik - Svijet Kupatila</a>
+        <ul class="nav navbar-nav">
+            <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> Početna</a></li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Web Shop <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Tuš Kabine</a></li>
+                    <li><a href="#">WC Solje</a></li>
+                    <li><a href="#">Električni Bojleri</a></li>
+                    <li><a href="#">Podni Sifoni</a></li>
+                </ul>
+            </li>
+            <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> Asortimani</a></li>
+
+            <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> Kontakt</a></li>
+        </ul>
+    </div>
+</nav>
+
+<!-- Header -->
+<div id="headerWrapper">
+    <div id="back-flower"></div>
+    <div id="logotext"></div>
+    <div id="fore-flower"></div>
+</div>
+
+
+<div class="container-fluid">
+    <!-- Left Sidebar -->
+    <div class="col-md-2">
+        Left side bar
+    </div>
+    <!-- Main Content -->
+    <div class="col-md-8">
+        <div class="row">
+        <h2 class="text-center">Istaknuti Proizvodi</h2>
+            <div class="col-md-3">
+                <h4>Tuš kabina</h4>
+                <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=Slika%20Proizvoda&w=230&h=250" alt="Tus kabina" class="img-thump" />
+                <p class="list-price text-danger">Cijena:<s>245.50 KM</s></p>
+                <p class="price">Nasa Cijena: 199.99 KM</p>
+                <button class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Detaljno</button>
+            </div>
+
+            <div class="col-md-3">
+                <h4>A-QA b 1S tuš set</h4>
+                <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=Slika%20Proizvoda&w=230&h=250" alt="Tus kabina" class="img-thump" />
+                <p class="list-price text-danger">Cijena:<s>245.50 KM</s></p>
+                <p class="price">Nasa Cijena: 199.99 KM</p>
+                <button class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Detaljno</button>
+            </div>
+
+            <div class="col-md-3">
+                <h4>A-QA eco ruža tuša</h4>
+                <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=Slika%20Proizvoda&w=230&h=250" alt="Tus kabina" class="img-thump"/>
+                <p class="list-price text-danger">Cijena:<s>245.50 KM</s></p>
+                <p class="price">Nasa Cijena: 199.99 KM</p>
+                <button class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Detaljno</button>
+            </div>
+
+            <div class="col-md-3">
+                <h4>A-QA zidni priključak</h4>
+                <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=Slika%20Proizvoda&w=230&h=250" alt="Tus kabina" class="img-thump"/>
+                <p class="list-price text-danger">Cijena:<s>245.50 KM</s></p>
+                <p class="price">Nasa Cijena: 199.99 KM</p>
+                <button class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Detaljno</button>
+            </div>
+
+            <div class="col-md-3">
+                <h4>Tuš kabina</h4>
+                <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=Slika%20Proizvoda&w=230&h=250" alt="Tus kabina" class="img-thump"/>
+                <p class="list-price text-danger">Cijena:<s>245.50 KM</s></p>
+                <p class="price">Nasa Cijena: 199.99 KM</p>
+                <button class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Detaljno</button>
+            </div>
+
+            <div class="col-md-3">
+                <h4>Tuš kabina</h4>
+                <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=Slika%20Proizvoda&w=230&h=250" alt="Tus kabina" class="img-thump"/>
+                <p class="list-price text-danger">Cijena:<s>245.50 KM</s></p>
+                <p class="price">Nasa Cijena: 199.99 KM</p>
+                <button class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Detaljno</button>
+            </div>
+
+            <div class="col-md-3">
+                <h4>Tuš kabina</h4>
+                <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=Slika%20Proizvoda&w=230&h=250" alt="Tus kabina" class="img-thump"/>
+                <p class="list-price text-danger">Cijena:<s>245.50 KM</s></p>
+                <p class="price">Nasa Cijena: 199.99 KM</p>
+                <button class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Detaljno</button>
+            </div>
+
+            <div class="col-md-3">
+                <h4>Tuš kabina</h4>
+                <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=Slika%20Proizvoda&w=230&h=250" alt="Tus kabina" class="img-thump"/>
+                <p class="list-price text-danger">Cijena:<s>245.50 KM</s></p>
+                <p class="price">Nasa Cijena: 199.99 KM</p>
+                <button class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Detaljno</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- Right Sidebar -->
+    <div class="col-md-2">Right Side Bar</div>
+</div>
+
+
+<!-- Footer -->
+<footer class="text-center" id="footer">&copy; Copyright 2016 - Banik - Svijet Kupatila </footer>
+
+
+
+<!-- Details Modal -->
+<div class="modal fade details-1" id="details1" tabindex="-1" role="dialog" aria-labelledby="details-1" aria-hidden="true">
+    
+</div>
+
+
+<!-- Parallax , maybe or maybe not! -->
+<script>
+    jQuery(window).scroll(function () {
+        var vscroll = jQuery(this).scrollTop();
+        jQuery('#logotext').css({
+            "transform": "translate(0px, " + vscroll / 2 + "px)"
+        });
+        var vscroll = jQuery(this).scrollTop();
+        jQuery('#back-flower').css({
+            "transform": "translate(" + vscroll / 5 + "px, -" + vscroll / 12 + "px)"
+        });
+        var vscroll = jQuery(this).scrollTop();
+        jQuery('#fore-flower').css({
+            "transform": "translate(0px, -" + vscroll / 2 + "px)"
+        });
+        //console.log(vscroll);
+    });
+</script>
+</body>
+</html>
